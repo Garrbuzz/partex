732acf4edee64c6333e42a71474f6e1634dcf52a
unknown <garrbuzz@gmail.com>
2020-10-12 20:04:18 +0300

    12-10-2020 catalog

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let options = {
-        duration: 180,
-        dist: -150,
-        shift: 0,
-        padding: 0,
-        fullWidth: false,
-        indicators: true,
-        noWrap: false
+    // let options = {
+    //     // duration: 180,
+    //     // dist: -150,
+    //     // shift: 0,
+    //     // padding: 0,
+    //     // fullWidth: false,
+    //     // indicators: true,
+    //     // noWrap: false
 
 
-    }
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+
+    // }
+
+    var elems = document.querySelector('.carousel');
+    var instances = M.Carousel.init(elems);
+    let la = document.querySelector('#left-arrow');
+    console.log(la);
+    let ra = document.querySelector('#right-arrow');
+    la.addEventListener("click", () => { instances.prev() });
+    ra.addEventListener('click', () => { instances.next() });
 });
